240069edc8ff711003e2e65d69c57cc08329b2e5
Tobias F. Petersen <tobsf@hotmail.com>
2017-01-27 10:02:30 +0100

    make til material komments below book info and add card css effect.

--- ting_object.tpl.php
+++ ting_object.tpl.php
@@ -10,15 +10,23 @@
  * - $content: Render array of content.
  */
 ?>
+<style>
+    .ting-object{
+        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+        transition: 0.3s;
+        padding: 5px;
+    }
+</style>
 <div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
     <?php 
+    echo render($content);
+    ?>
+    <?php
     if(isset($material_description) && $material_description != "")
     {
-        echo t('<br/><h4><b>Bibliotekarens noter:</b></h4> ') .$material_description;
-        echo "<br/>";
+        echo t('<div><br/><h4><b>Bibliotekarens noter:</b></h4> ') .$material_description;
+        echo "<br/></div>";
     }
-    echo render($content);
-    
     ?>
 
 </div>
