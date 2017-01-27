5d7f749c94e5d291df31b7fd64b328189faf6a74
Tobias F. Petersen <tobsf@hotmail.com>
2017-01-27 10:38:46 +0100

    styling problem

--- ting_object.tpl.php
+++ ting_object.tpl.php
@@ -10,13 +10,7 @@
  * - $content: Render array of content.
  */
 ?>
-<style>
-    .ting-object{
-        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-        transition: 0.3s;
-        padding: 5px;
-    }
-</style>
+
 <div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
     <?php 
     echo render($content);
@@ -24,6 +18,15 @@
     <?php
     if(isset($material_description) && $material_description != "")
     {
+    ?>
+    <style>
+        .ting-object{
+            box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+            transition: 0.3s;
+            padding: 5px;
+        }
+    </style>
+    <?php
         echo "<div>";
         echo t('<br/><h4><b>Bibliotekarens noter:</b></h4> ') .$material_description;
         echo "<br/></div>";
